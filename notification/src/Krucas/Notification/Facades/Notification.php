a75cdf31e8f637ba079c43e543ca9ac8f24e6f59
Bernd Obendorfer <bernd.obendorfer@chello.at>
2014-08-28 13:41:46 +0200

    edvinaskrucas IROCODE

---
+++ notification/src/Krucas/Notification/Facades/Notification.php
@@ -0,0 +1,20 @@
+<?php namespace Krucas\Notification\Facades;
+
+use Illuminate\Support\Facades\Facade;
+
+class Notification extends Facade
+{
+    /**
+     * Get the registered component.
+     *
+     * @return object
+     */
+    protected static function getFacadeAccessor()
+    {
+
+   
+
+
+        return 'notification';
+    }
+}
